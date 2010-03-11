ff1adec345df7624e654cf7304290da51438ebfc
Joshua Hughes <kivhift@gmail.com>
2010-03-11 09:27:18 -0500

    Add a convert command.
    I thought that it would be useful to have some sort of ability to convert
    values with given units into other compatible units.  I already have a
    conversion package and thus it was easy to add this command in.  It simply
    takes a value, its unit and the desired unit and then displays the
    resulting conversion value if all's well.

--- commands.py
+++ commands.py
@@ -190,6 +190,46 @@ class WundergroundCommand(qmk.Command):
 		query = self.__baseURL % urllib.quote_plus(text.encode('utf-8'))
 		webbrowser.open_new_tab(query)
 
+class ConvertCommand(qmk.Command):
+	'''Convert values with given units to other units.'''
+	def __init__(self):
+		from conversion.pressure import Pressure
+		from conversion.temperature import Temperature
+		from conversion.volume import Volume
+
+		self._name = 'convert'
+		self._help = self.__doc__
+
+		self.__P = Pressure()
+		self.__T = Temperature()
+		self.__V = Volume()
+
+	def action(self, arg):
+		if arg is None: return
+		arg = arg.split()
+		if len(arg) != 3: return
+		try:
+			val = float(arg[0])
+		except:
+			return
+		fu, tu = arg[1], arg[2]
+		if fu in self.__P.available_units():
+			co = self.__P
+		elif fu in self.__T.available_units():
+			co = self.__T
+		elif fu in self.__V.available_units():
+			co = self.__V
+		else:
+			return
+
+		if not tu in co.available_units():
+			return
+
+		setattr(co, fu, val)
+
+		qmk.Message.get()('%s %s --> %f %s' % (
+			arg[0], fu, getattr(co, tu), tu))
+
 def commands():
 	cmds = []
 	cmds.append(GoogleCommand())
@@ -202,4 +242,5 @@ def commands():
 	cmds.append(OctopartCommand())
 	cmds.append(WundergroundCommand())
 	cmds.append(EvalCommand())
+	cmds.append(ConvertCommand())
 	return cmds
