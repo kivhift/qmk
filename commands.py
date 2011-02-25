1f3d9b62b48427eee3bb93f3003908968ba380fc
Joshua Hughes <kivhift@gmail.com>
2011-02-25 15:39:36 -0500

    Add computer command.
    This command can be used to hibernate, suspend, shutdown or reboot the
    computer or log off the user.

--- commands.py
+++ commands.py
@@ -6,6 +6,7 @@ import webbrowser
 import datetime
 import calendar
 import subprocess
+import ctypes
 
 # For eval command
 from math import *
@@ -286,6 +287,40 @@ class ConvertCommand(qmk.Command):
         qmk.Message()('%s %s --> %f %s' % (
             arg[0], fu, getattr(co, tu), tu))
 
+class ComputerCommand(qmk.Command):
+    '''Hibernate, suspend, shutdown or reboot the computer or log off.'''
+    # These are from winuser.h.
+    EWX_LOGOFF = 0
+    EWX_POWEROFF = 8
+    EWX_REBOOT = 2
+    EWX_FORCEIFHUNG = 16
+    def __init__(self):
+        self._name = 'computer'
+        self._help = self.__doc__
+
+    def hibernate(self):
+        ctypes.windll.powrprof.SetSuspendState(1, 0, 0)
+
+    def suspend(self):
+        ctypes.windll.powrprof.SetSuspendState(0, 0, 0)
+
+    def shutdown(self):
+        ctypes.windll.user32.ExitWindowsEx(ComputerCommand.EWX_POWEROFF, 0)
+
+    def reboot(self):
+        ctypes.windll.user32.ExitWindowsEx(ComputerCommand.EWX_REBOOT, 0)
+
+    def logoff(self):
+        ctypes.windll.user32.ExitWindowsEx(ComputerCommand.EWX_LOGOFF, 0)
+
+    def action(self, arg):
+        if arg is None: return
+
+        for a in 'hibernate suspend shutdown reboot logoff'.split():
+            if a.startswith(arg): return getattr(self, a)()
+
+        qmk.ErrorMessage()('Invalid request: %s' % arg)
+
 def commands():
     cmds = []
     cmds.append(GoogleCommand())
@@ -301,4 +336,5 @@ def commands():
     cmds.append(ConvertCommand())
     cmds.append(ClipboardCommand())
     cmds.append(AmazonCommand())
+    cmds.append(ComputerCommand())
     return cmds
