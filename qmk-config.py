a15da7549579e4a6f56a542a7f5ffde593ef7f95
Joshua Hughes <kivhift@gmail.com>
2011-02-28 12:05:29 -0500

    Add example configuration file.

---
+++ qmk-config.py
@@ -0,0 +1,18 @@
+commands = [
+    ('commands', '''
+        amazon.py
+        beolingus.py
+        browse.py
+        cal.py
+        clipboard.py
+        computer.py
+        convert.py
+        date.py
+        eval.py
+        google.py
+        help.py
+        octopart.py
+        run.py
+        wunderground.py
+    '''.split())
+]
