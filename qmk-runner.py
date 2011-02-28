2d79a785f71ffc01e03697e1fba8965506f201b4
Joshua Hughes <kivhift@gmail.com>
2011-02-28 11:15:01 -0500

    Add the ability to have config. file and load command files.
    On top off adding the configuration file, I've moved away from having a
    single module with all of the commands that loaded from the script
    directory to having multiple user-specifiable command-module
    directories/files that are loaded based on the configuration.

--- qmk-runner.py
+++ qmk-runner.py
@@ -2,14 +2,13 @@
 
 import os
 import sys
-import glob
+import imp
+import optparse
 
-from PyQt4 import QtCore
-from PyQt4 import QtGui
-from PyQt4 import QtXml
-from PyQt4 import QtSvg
+from PyQt4 import QtCore, QtGui, QtXml, QtSvg
 
 import qmk
+import utils
 
 class QuitCommand(qmk.Command):
     '''Use this command to quit.'''
@@ -21,7 +20,37 @@ class QuitCommand(qmk.Command):
         # XXX Perhaps add some fancier shutdown stuff here.
         QtGui.qApp.quit()
 
-app = QtGui.QApplication([])
+modloadtuple = ('.py', 'rb', imp.PY_SOURCE)
+def load_qmkconfig(filename):
+    with open(filename, 'rb') as f:
+        cfg = imp.load_module('qmkconfig', f, filename, modloadtuple)
+
+    return cfg
+
+def register_commands(cmds):
+    for t in cmds:
+        basedir = t[0]
+        if not basedir:
+            basedir = os.path.join(
+                utils.get_user_info()['HOME'], '.qmk', 'commands')
+        for fn in t[1]:
+            filename = os.path.join(basedir, fn)
+            modname = os.path.splitext(os.path.basename(fn))[0]
+            with open(filename, 'rb') as f:
+                c = imp.load_module(modname, f, filename, modloadtuple)
+            qmk.CommandManager().registerCommands(c.commands())
+
+optpar = optparse.OptionParser()
+optpar.add_option('-f', '--filename', dest = 'filename', default = None,
+    help = 'name of config file to use instead of ~/.qmk/qmkconfig.py')
+
+app = QtGui.QApplication(sys.argv)
+opts, args = optpar.parse_args()
+
+qmkconfig = load_qmkconfig(opts.filename if opts.filename is not None
+    else os.path.join(utils.get_user_info()['HOME'], '.qmk', 'qmkconfig.py'))
+register_commands(qmkconfig.commands)
+
 app.setQuitOnLastWindowClosed(False)
 app.setWindowIcon(QtGui.QIcon(':images/qmk-icon.svg'))
 # {{{
@@ -57,16 +86,14 @@ QListView {
 ''')
 # }}}
 
+qmk.CommandManager().registerCommands([QuitCommand()])
+qmk.CommandInput().updateCompletions()
+
 qmk.InputFilter().setQMKKeyboardCallback(qmk.Engine.QMKCallback)
 qmk.InputFilter().enableQMKKeyboardCallback()
 qmk.InputFilter().setKeyboardCallback(qmk.Engine.hideMessageCallback)
 qmk.InputFilter().setMouseCallback(qmk.Engine.hideMessageCallback)
 qmk.InputFilter().setKeyboardWindowId(int(qmk.CommandInput().winId()))
-
-qmk.CommandManager().registerCommands([QuitCommand()])
-
-qmk.CommandManager().registerCommands(__import__('commands').commands())
-qmk.CommandInput().updateCompletions()
 
 qmk.Message()('QMK has started...')
 
