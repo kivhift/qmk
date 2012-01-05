d493d67d5434fd3f6c5a1c8e111e4a6ed25bde78
Joshua Hughes <kivhift@gmail.com>
2012-01-05 15:56:38 -0500

    Add UPS command for tracking packages.

---
+++ src/commands/ups.py
@@ -0,0 +1,22 @@
+import urllib
+import webbrowser
+
+import qmk
+
+class UPSCommand(qmk.Command):
+    '''Use the UPS website to view the tracking info for the given tracking
+    number.'''
+    def __init__(self):
+        self._name = 'ups'
+        self._help = self.__doc__
+        self.__baseURL = 'http://wwwapps.ups.com/WebTracking/' \
+            'track?HTMLVersion=5.0&loc=en_US&Requester=UPSHome&' \
+            'WBPM_lid=homepage%%2Fct1.html_pnl_trk&trackNums=%s&track.x=Track'
+
+    @qmk.Command.actionRequiresArgument
+    def action(self, arg):
+        query = self.__baseURL % urllib.quote_plus(
+            arg.split()[0].encode('utf-8'))
+        webbrowser.open_new_tab(query)
+
+def commands(): return [ UPSCommand() ]
